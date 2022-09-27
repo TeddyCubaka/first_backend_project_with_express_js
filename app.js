6f1c41cbe03e2e340267c9b17a3ce39392e2d30c
Teddy Cubaka <teddycubaka96@gmail.com>
2022-09-27 12:15:53 +0100

    first real request on database, create and read on frontend space

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Thing = require('./models/thing');
 
 const app = express();
+
 app.use(express.json());
-
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://Teddy:birhingingwa@teddy.miua6qr.mongodb.net/?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res, next)=>{
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -18,38 +13,33 @@ app.use((req, res, next)=>{
   next();
 })
 
-app.post('/api/stuff', (req, res, next) => {
-  console.log(req.body);
-  res.status(200).json({
-    message : "Réponse reçu"
-  })
-})
+mongoose.connect('mongodb+srv://Teddy:birhingingwa@teddy.miua6qr.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.use('/api/stuff', (req, res, next) => {
-  const stuff = [
-    {
-      _id: 'oeihfzeoi',
-      title: 'Mon premier objet',
-      description: 'Les infos de mon premier objet',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      price: 4900,
-      userId: 'qsomihvqios',
-    },
-    {
-      _id: 'oeihfzeomoihi',
-      title: 'Mon deuxième objet',
-      description: 'Les infos de mon deuxième objet',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      price: 2900,
-      userId: 'qsomihvqios',
-    }
-  ];
-  res.status(200).json(stuff);
+app.post('/api/stuff', (req, res, next) => {
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body
+  });
+  thing.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
   next();
 });
 
-// mongodb+srv://Teddy:<password>@teddy.miua6qr.mongodb.net/?retryWrites=true&w=majority
+app.get('/api/stuff/:id', (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(404).json({ error }))
+})
 
-
+app.get('/api/stuff', (req, res, next) => {
+  Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }))
+});
 
 module.exports = app;
