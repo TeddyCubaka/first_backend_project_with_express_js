944d7322722f7d121ce3f25ea7bbec7edd985aef
Teddy Cubaka <teddycubaka96@gmail.com>
2022-10-05 18:28:18 +0100

    add controllers of all method

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Thing = require('../models/thing');
+const stuffCtrl = require('../controlers/stuff');
 
 router.use((req, res, next)=>{
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -9,39 +9,15 @@ router.use((req, res, next)=>{
     next();
 })
 
-router.post('/', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
-        ...req.body
-});
-    thing.save()
-        .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-        .catch(error => res.status(400).json({ error }));
-    next();
-});
+router.post('/', stuffCtrl.createThing );
 
-router.get('/:id', (req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
-        .then(thing => res.status(200).json(thing))
-        .catch(error => res.status(404).json({ error }))
-})
+router.get('/', stuffCtrl.findAllThings);
 
-router.put('/:id', (req, res, next) => {
-    Thing.updateOne({ _id: req.params.id }, {...req.body ,_id : req.params.id})
-        .then(() => res.status(200).json({message : "objet modifié"}))
-        .catch(error => res.status(404).json({ error }))
-})
+router.get('/:id', stuffCtrl.findOneThing)
 
-router.delete('/:id', (req, res, next) => {
-    Thing.deleteOne({_id: req.params.id})
-        .then(() => res.status(200).json({message: "object supprimé !"}))
-        .catch(error => res.status(404).json({ error }))
-})
+router.put('/:id', stuffCtrl.updateThing)
 
-router.get('/', (req, res, next) => {
-    Thing.find()
-        .then(things => res.status(200).json(things))
-        .catch(error => res.status(400).json({ error }))
-});
+router.delete('/:id', stuffCtrl.deleteThing)
+
 
 module.exports = router;
