bcc851941cf9baaba865aeb5bdcf7661845308fd
Teddy Cubaka <teddycubaka96@gmail.com>
2022-10-07 15:28:03 +0100

    add the sign up function

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const userCtrl = require('../controlers/user');
+
+router.post('/singup', userCtrl.singup);
+router.post('/login', userCtrl.login);
 
 module.exports = router;
