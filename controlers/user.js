d35a7e5ff8a1e9bf4ec9d2feeea4262a71f00a3c
Teddy Cubaka <teddycubaka96@gmail.com>
2022-10-07 16:30:05 +0100

    add a realy token with jsonwebtoken

--- controlers/user.js
+++ controlers/user.js
@@ -1,5 +1,8 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
+
 const User = require('../models/user')
+
 
 
 exports.singup = (req, res) => {
@@ -17,5 +20,28 @@ exports.singup = (req, res) => {
 }
 
 exports.login = (req, res) => {
-    
+    User.findOne({email : req.body.email})
+        .then((user) => {
+            if(!user){
+                return res.status(401).json({message : "Incorrect password !"})
+            }
+            bcrypt.compare(req.body.password, user.password)
+                .then(valid => {
+                    if(!valid){
+                        return res.status(401).json({message : "Incorrect password !"})
+                    }
+                    res.status(200).json({
+                        userId : user._id,
+                        token : jwt.sign(
+                            {userId : user._id},
+                            "RANDOM_TOKEN_KEYS",
+                            {
+                                expiresIn : '24h'
+                            }
+                        )
+                    })
+                })
+                .catch(err => res.status(500).json({ err }))
+        })
+        .catch(err => res.status(500).json({ err}))
 }
